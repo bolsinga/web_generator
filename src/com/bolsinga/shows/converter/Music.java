2fa0fb692fd303903dc1af069bbd70830abbfd0a
Greg Bolsinga <greg@bolsinga.com>
2004-01-26 05:55:06 +0000

    outputs limited venue, artist, performance, and shows.
    git-svn-id: file:///svn/repository/web_generator/trunk@1062 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/shows/converter/Music.java
+++ src/com/bolsinga/shows/converter/Music.java
@@ -55,11 +55,11 @@ public class Music {
 
 			createRelations(objFactory, music, relations);
 		
-			List mShows = music.getShow();
-			convert(objFactory, music, shows, mShows);
+			convert(objFactory, music, shows);
 			dump(music);
 		} catch (JAXBException e) {
 			System.err.println(e);
+			e.printStackTrace();
 			System.exit(1);
 		}
 	}
@@ -94,7 +94,7 @@ public class Music {
 			
 			mVenues.add(xVenue);
 			
-			sVenues.put(name, xVenue.getId());
+			sVenues.put(name, xVenue);
 		}
 	}
 	
@@ -113,7 +113,42 @@ public class Music {
 	
 	}
 	
-	private static com.bolsinga.music.Artist addArtist(ObjectFactory objFactory, String name) throws JAXBException {
+	private static com.bolsinga.music.Date createDate(ObjectFactory objFactory, String date) throws JAXBException {
+		com.bolsinga.music.Date result = objFactory.createDate();
+		
+		String monthString, dayString, yearString = null;
+		int month, day, year = 0;
+		
+		StringTokenizer st = new StringTokenizer(date, "-");
+		
+		monthString = st.nextToken();
+		dayString = st.nextToken();
+		yearString = st.nextToken();
+		
+		month = Integer.parseInt(monthString);
+		day = Integer.parseInt(dayString);
+		year = Integer.parseInt(yearString);
+		
+		if ((month == 0) || (day == 0) || (year == 1900)) {
+			result.setUnknown(true);
+		}
+		
+		if (month != 0) {
+			result.setMonth(new java.math.BigInteger(monthString));
+		}
+		
+		if (day != 0) {
+			result.setDay(new java.math.BigInteger(dayString));
+		}
+		
+		if (year != 1900) {
+			result.setYear(new java.math.BigInteger(yearString));
+		}
+		
+		return result;
+	}
+	
+	private static com.bolsinga.music.Artist addArtist(ObjectFactory objFactory, com.bolsinga.music.Music music, String name) throws JAXBException {
 		com.bolsinga.music.Artist result = null;
 		if (!sArtists.containsKey(name)) {
 			result = objFactory.createArtist();
@@ -122,6 +157,7 @@ public class Music {
 			if (sBandSorts.containsKey(name)) {
 				result.setSortname((String)sBandSorts.get(name));
 			}
+			music.getArtist().add(result);
 			sArtists.put(name, result);
 		} else {
 			result = (com.bolsinga.music.Artist)sArtists.get(name);
@@ -129,16 +165,22 @@ public class Music {
 		return result;
 	}
 	
-	private static com.bolsinga.music.Performance addPerformance(ObjectFactory objFactory, String name, String date) throws JAXBException {
-	
-		com.bolsinga.music.Artist artist = addArtist(objFactory, name);
-		
-		com.bolsinga.music.Performance perf = objFactory.createPerformance();
+	static int sPerformanceID = 0;
 
-		return null;
+	private static com.bolsinga.music.Performance createPerformance(ObjectFactory objFactory, com.bolsinga.music.Music music, String name, com.bolsinga.music.Date date) throws JAXBException {
+			
+		com.bolsinga.music.Artist xArtist = addArtist(objFactory, music, name);
+				
+		com.bolsinga.music.Performance perf = objFactory.createPerformance();
+		
+		perf.setArtist(xArtist);
+		perf.setDate(date);
+		perf.setId("performance_" + sPerformanceID++);
+		
+		return perf;
 	}
 		
-	private static void convert(ObjectFactory objFactory, com.bolsinga.music.Music music, List shows, List mShows) throws JAXBException {
+	private static void convert(ObjectFactory objFactory, com.bolsinga.music.Music music, List shows) throws JAXBException {
 		// Go through each show.
 		//  Create an Artist for each band in the set, if it doesn't already exist. Use the sort name.
 		//  Create a Date.
@@ -150,29 +192,41 @@ public class Music {
 		Show oldShow = null;
 		com.bolsinga.music.Show xShow = null;
 		com.bolsinga.music.Artist xArtist = null;
+		com.bolsinga.music.Performance xPerf = null;
+		com.bolsinga.music.Date xDate = null;
+
+		int index = 0;
 		
 		List oldBands = null;
 		ListIterator bi = null;
 		String oldBand = null;
 		
-		List xArtists = music.getArtist();
-		
 		ListIterator li = shows.listIterator();
 		while (li.hasNext()) {
 			oldShow = (Show)li.next();
+			
+			xShow = objFactory.createShow();
+			
+			xDate = createDate(objFactory, oldShow.getDate());
+			xShow.setDate(xDate);
 			
 			oldBands = oldShow.getBands();
 			bi = oldBands.listIterator();
 			while (bi.hasNext()) {
 				oldBand = (String)bi.next();
+								
+				xPerf = createPerformance(objFactory, music, oldBand, xDate);
+				music.getPerformance().add(xPerf);
 				
-				xArtist = addArtist(objFactory, oldBand);
+				xShow.getPerformance().add(xPerf);
 			}
+
+			xShow.setVenue(sVenues.get(oldShow.getVenue()));
 			
-//			mShows.add(xShow);
+			xShow.setId("show_" + index++);
+			
+			music.getShow().add(xShow);
 		}
-		
-		xArtists.addAll(sArtists.values());
 	}
 	
 	private static void dump(com.bolsinga.music.Music music) throws JAXBException {
