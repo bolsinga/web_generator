f5ec0c58423ae6ba4a4b6ec43e114c00f1845ef1
Greg Bolsinga <greg@bolsinga.com>
2004-01-30 19:51:25 +0000

    Add empty Web target and source for generating web site.
    git-svn-id: file:///svn/repository/web_generator/trunk@1074 a1e501d5-4615-0410-8dc9-a46690435e56

---
+++ src/com/bolsinga/music/web/Web.java
@@ -0,0 +1,17 @@
+package com.bolsinga.music.web;
+
+public class Web {
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Usage: Web [source.xml] [output.dir]");
+			System.exit(0);
+		}
+		
+		Web.generate(args[0], args[1]);
+	}
+	
+	public static void generate(String sourceFile, String outputDir) {
+	
+	}
+}
