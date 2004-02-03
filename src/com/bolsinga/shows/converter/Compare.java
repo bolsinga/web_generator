9eb2532a9df37834aa365ee348f5ea5861345d7d
Greg Bolsinga <greg@bolsinga.com>
2004-02-03 06:33:39 +0000

    add raw data Comparators.
    git-svn-id: file:///svn/repository/web_generator/trunk@1080 a1e501d5-4615-0410-8dc9-a46690435e56

---
+++ src/com/bolsinga/shows/converter/Compare.java
@@ -0,0 +1,47 @@
+package com.bolsinga.shows.converter;
+
+import java.util.*;
+
+public class Compare {
+	private static int convert(String d) {
+		String monthString, dayString, yearString = null;
+		int month, day, year = 0;
+		
+		StringTokenizer st = new StringTokenizer(d, "-");
+		
+		monthString = st.nextToken();
+		dayString = st.nextToken();
+		yearString = st.nextToken();
+		
+		month = Integer.parseInt(monthString);
+		day = Integer.parseInt(dayString);
+		year = Integer.parseInt(yearString);
+
+		return (year * 10000) + (month * 100) + day;
+	}
+
+	public static final Comparator VENUE_COMPARATOR = new Comparator() {
+		public int compare(Object o1, Object o2) {
+			Venue r1 = (Venue)o1;
+			Venue r2 = (Venue)o2;
+			
+			return r1.getName().compareToIgnoreCase(r2.getName());
+		}
+	};
+	
+	public static final Comparator SHOW_COMPARATOR = new Comparator() {
+		public int compare(Object o1, Object o2) {
+			Show r1 = (Show)o1;
+			Show r2 = (Show)o2;
+			
+			int result = convert(r1.getDate()) - convert(r2.getDate());
+			if (result == 0) {
+				result = r1.getVenue().compareToIgnoreCase(r2.getVenue());
+				if (result == 0) {
+					result = ((String)r1.getBands().get(0)).compareToIgnoreCase(((String)r2.getBands().get(0)));
+				}
+			}
+			return result;
+		}
+	};
+}
