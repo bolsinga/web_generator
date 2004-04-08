01f268b558cb57e2c2d74a961f4d48aa553ee2f3
Greg Bolsinga <greg@bolsinga.com>
2004-04-08 05:13:55 +0000

    add Data to Month String converter.
    git-svn-id: file:///svn/repository/web_generator/trunk@1108 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/music/util/Util.java
+++ src/com/bolsinga/music/util/Util.java
@@ -1,6 +1,27 @@
 package com.bolsinga.music.util;
 
+import java.util.*;
+
 public class Util {
+	static Vector sMonths = null;
+	
+	static {
+		sMonths = new Vector();
+		sMonths.add("Unknown");
+		sMonths.add("January");
+		sMonths.add("February");
+		sMonths.add("March");
+		sMonths.add("April");
+		sMonths.add("May");
+		sMonths.add("June");
+		sMonths.add("July");
+		sMonths.add("August");
+		sMonths.add("September");
+		sMonths.add("October");
+		sMonths.add("November");
+		sMonths.add("December");
+	}
+	
 	public static String toString(com.bolsinga.music.data.Date date) {
 		StringBuffer sb = new StringBuffer();
 		
@@ -11,5 +32,9 @@ public class Util {
 		sb.append((date.getYear() != null) ? date.getYear().intValue() : 0);
 
 		return sb.toString();
+	}
+	
+	public static String toMonth(com.bolsinga.music.data.Date date) {
+		return (date.getMonth() != null) ? (String)sMonths.get(date.getMonth().intValue()) : "Unknown";
 	}
 }
