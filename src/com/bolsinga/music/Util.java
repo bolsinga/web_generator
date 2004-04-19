36acfa81b080d09b2f2824c8c616bddeddc5a3b0
Greg Bolsinga <greg@bolsinga.com>
2004-04-19 02:48:06 +0000

    Use java DateFormatter, and handle Unknown dates with explicit text.
    git-svn-id: file:///svn/repository/web_generator/trunk@1114 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/music/Util.java
+++ src/com/bolsinga/music/Util.java
@@ -1,40 +1,53 @@
 package com.bolsinga.music.util;
 
+import java.text.*;
 import java.util.*;
 
 public class Util {
-	static Vector sMonths = null;
-	
-	static {
-		sMonths = new Vector();
-		sMonths.add("Unknown");
-		sMonths.add("January");
-		sMonths.add("February");
-		sMonths.add("March");
-		sMonths.add("April");
-		sMonths.add("May");
-		sMonths.add("June");
-		sMonths.add("July");
-		sMonths.add("August");
-		sMonths.add("September");
-		sMonths.add("October");
-		sMonths.add("November");
-		sMonths.add("December");
-	}
-	
-	public static String toString(com.bolsinga.music.data.Date date) {
-		StringBuffer sb = new StringBuffer();
-		
-		sb.append((date.getMonth() != null) ? date.getMonth().intValue() : 0);
-		sb.append("/");
-		sb.append((date.getDay() != null) ? date.getDay().intValue() : 0);
-		sb.append("/");
-		sb.append((date.getYear() != null) ? date.getYear().intValue() : 0);
 
-		return sb.toString();
+	static DateFormat sMonthFormat = new SimpleDateFormat("MMMM");
+	static DateFormat sWebFormat = new SimpleDateFormat("M/d/yyyy");
+	
+	public static Calendar toCalendar(com.bolsinga.music.data.Date date) {
+		Calendar d = Calendar.getInstance();
+		if (!date.isUnknown()) {
+			d.set(date.getYear().intValue(), date.getMonth().intValue() - 1, date.getDay().intValue());
+		} else {
+			System.err.println("Can't convert Unknown com.bolsinga.music.data.Date");
+			System.exit(1);
+		}
+		return d;
+	}
+
+	public static String toString(com.bolsinga.music.data.Date date) {
+		if (!date.isUnknown()) {
+			return sWebFormat.format(toCalendar(date).getTime());
+		} else {
+			StringBuffer sb = new StringBuffer();
+			
+			sb.append((date.getMonth() != null) ? date.getMonth().intValue() : 0);
+			sb.append("/");
+			sb.append((date.getDay() != null) ? date.getDay().intValue() : 0);
+			sb.append("/");
+			sb.append((date.getYear() != null) ? date.getYear().intValue() : 0);
+			
+			sb.append(" (Unknown)");
+			
+			return sb.toString();
+		}
 	}
 	
 	public static String toMonth(com.bolsinga.music.data.Date date) {
-		return (date.getMonth() != null) ? (String)sMonths.get(date.getMonth().intValue()) : "Unknown";
+		if (!date.isUnknown()) {
+			return sMonthFormat.format(toCalendar(date).getTime());
+		} else {
+			Calendar d = Calendar.getInstance();
+			if (date.getMonth() != null) {
+				d.set(Calendar.MONTH, date.getMonth().intValue() - 1);
+				return sMonthFormat.format(d.getTime());
+			} else {
+				return "Unknown";
+			}
+		}
 	}
 }
