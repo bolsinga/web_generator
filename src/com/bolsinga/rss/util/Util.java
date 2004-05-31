745f9eb74bfaa6b42e8adbce12ff8f00866f68f4
Greg Bolsinga <greg@bolsinga.com>
2004-05-31 00:05:10 +0000

    Strip URLs from RSS feeds.
    git-svn-id: file:///svn/repository/web_generator/trunk@1242 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/rss/util/Util.java
+++ src/com/bolsinga/rss/util/Util.java
@@ -1,6 +1,7 @@
 package com.bolsinga.rss.util;
 
 import java.text.*;
+import java.util.regex.*;
 
 public class Util {
 	private static DateFormat sRSSDateFormat = new SimpleDateFormat("EEE, dd MMM yyy HH:mm:ss z");
@@ -9,7 +10,32 @@ public class Util {
 		return sRSSDateFormat.format(date);
 	}
 
+	private static final Pattern sHTMLTag = Pattern.compile("<([a-z][a-z0-9]*)[^>]*>([^<]*)</\\1>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+	private static String removeLinks(String data) {
+		StringBuffer sb = new StringBuffer();
+		
+		Matcher match = sHTMLTag.matcher(data);
+		while (match.find()) {
+			match.appendReplacement(sb, "$2");
+		}
+		match.appendTail(sb);
+		
+		return sb.toString();
+	}
+	
 	public static String createDescription(String data, int maxlength) {
-		return (data.length() > maxlength) ? data.substring(0, maxlength) : data;
+		// This should strip out URLs, keeping only the regular data.
+		String linkFree = removeLinks(data);
+		
+		// This truncate at the nearest word after maxlength characters, and add an ellipsis if necessary.
+		StringBuffer sb = new StringBuffer();
+		if (linkFree.length() > maxlength) {
+			sb.append(linkFree.substring(0, maxlength));
+			sb.append("É");
+		} else {
+			sb.append(linkFree);
+		}
+		return sb.toString();
 	}
 }
