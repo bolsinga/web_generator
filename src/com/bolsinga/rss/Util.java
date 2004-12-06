069e861f84e5c975623b2759a3fb5a6025874bbb
Greg Bolsinga <greg@bolsinga.com>
2004-12-06 16:46:29 +0000

    Don't strip URLs from RSS summaries for the diary entries. Also insert <p> for newlines.
    git-svn-id: file:///svn/repository/web_generator/trunk@1326 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/rss/Util.java
+++ src/com/bolsinga/rss/Util.java
@@ -10,25 +10,14 @@ public class Util {
 		return sRSSDateFormat.format(date);
 	}
 
-	private static final Pattern sHTMLTag = Pattern.compile("<([a-z][a-z0-9]*)[^>]*>([^<]*)</\\1>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-
-	private static String removeLinks(String data) {
-		StringBuffer sb = new StringBuffer();
-		
-		if (data != null) {
-			Matcher match = sHTMLTag.matcher(data);
-			while (match.find()) {
-				match.appendReplacement(sb, "$2");
-			}
-			match.appendTail(sb);
-		}
-		
-		return sb.toString();
+	private static Pattern sNewlineEncoding = Pattern.compile("\n", Pattern.DOTALL);
+	
+	private static String encodeNewlines(String data) {
+		return sNewlineEncoding.matcher(data).replaceAll("<p>");
 	}
 	
 	public static String createDescription(String data, int maxlength) {
-		// This should strip out URLs, keeping only the regular data.
-		String linkFree = removeLinks(data);
+		String linkFree = encodeNewlines(data);
 		
 		// This truncate at the nearest word after maxlength characters, and add an ellipsis if necessary.
 		StringBuffer sb = new StringBuffer();
