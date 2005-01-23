7b4f9cc48a27f8b9d1acb1547794cb957b09ba2f
Greg Bolsinga <greg@bolsinga.com>
2005-01-23 21:08:22 +0000

    clean up columns
    git-svn-id: file:///svn/repository/web_generator/trunk@1481 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/web/util/CSS.java
+++ src/com/bolsinga/web/util/CSS.java
@@ -3,55 +3,55 @@ package com.bolsinga.web.util;
 import java.io.*;
 
 public class CSS {
-    public static final String ARTIST_INDEX         = "artist_index";
-    public static final String ARTIST_ITEM          = "artist_item";
-    public static final String ARTIST_RELATION      = "artist_relation";
-    public static final String ARTIST_SHOW          = "artist_show";
-    public static final String ARTIST_TRACKS        = "artist_tracks";
-
-    public static final String DIARY_ENTRY          = "diary_entry";
-    public static final String DIARY_HEADER         = "diary_header";
-    public static final String DIARY_INDEX          = "diary_index";
-    public static final String DIARY_MENU           = "diary_menu";
-
-    public static final String INTERNAL                     = "int";
-
-    public static final String MAIN_COL1            = "main_col1";
-    public static final String MAIN_COL2            = "main_col2";
-    public static final String MAIN_DIARY           = "main_diary";
-    public static final String MAIN_HEADER          = "main_header";
-    public static final String MAIN_MAIN            = "main_main";
-    public static final String MAIN_STATIC          = "main_static";
-    public static final String MAIN_LINKS           = "main_links";
-
-    public static final String DOC_MAIN         = "doc_main";
-    public static final String DOC_SUB          = "doc_sub";
-
-    public static final String MUSIC_HEADER         = "music_header";
-    public static final String MUSIC_MENU           = "music_menu";
-
-    public static final String PREVIEW_MAIN         = "preview_main";
-    public static final String PREVIEW_MENU         = "preview_menu";
-    public static final String PREVIEW_RECENT       = "preview_recent";
-    public static final String PREVIEW_SHOW         = "preview_show";
-
-    public static final String SHOW_COMMENT         = "show_comment";
-    public static final String SHOW_INDEX           = "show_index";
-    public static final String SHOW_ITEM            = "show_item";
-
-    public static final String TRACKS_INDEX         = "tracks_index";
-    public static final String TRACKS_ITEM          = "tracks_item";
-    public static final String TRACKS_MENU          = "tracks_menu";
-
-    public static final String VENUE_INDEX          = "venue_index";
-    public static final String VENUE_ITEM           = "venue_item";
-    public static final String VENUE_RELATION       = "venue_relation";
-    public static final String VENUE_SHOW           = "venue_show";
-    
-    public static final String TABLE_HEADER     = "table_header";
-    public static final String TABLE_ROW        = "table_row";
-    public static final String TABLE_ROW_ALT    = "table_row_alt";
-    public static final String TABLE_FOOTER     = "table_footer";
+    public static final String ARTIST_INDEX    = "artist_index";
+    public static final String ARTIST_ITEM     = "artist_item";
+    public static final String ARTIST_RELATION = "artist_relation";
+    public static final String ARTIST_SHOW     = "artist_show";
+    public static final String ARTIST_TRACKS   = "artist_tracks";
+                                               
+    public static final String DIARY_ENTRY     = "diary_entry";
+    public static final String DIARY_HEADER    = "diary_header";
+    public static final String DIARY_INDEX     = "diary_index";
+    public static final String DIARY_MENU      = "diary_menu";
+                                               
+    public static final String INTERNAL        = "int";
+                                               
+    public static final String MAIN_COL1       = "main_col1";
+    public static final String MAIN_COL2       = "main_col2";
+    public static final String MAIN_DIARY      = "main_diary";
+    public static final String MAIN_HEADER     = "main_header";
+    public static final String MAIN_MAIN       = "main_main";
+    public static final String MAIN_STATIC     = "main_static";
+    public static final String MAIN_LINKS      = "main_links";
+                                               
+    public static final String DOC_MAIN        = "doc_main";
+    public static final String DOC_SUB         = "doc_sub";
+                                               
+    public static final String MUSIC_HEADER    = "music_header";
+    public static final String MUSIC_MENU      = "music_menu";
+                                               
+    public static final String PREVIEW_MAIN    = "preview_main";
+    public static final String PREVIEW_MENU    = "preview_menu";
+    public static final String PREVIEW_RECENT  = "preview_recent";
+    public static final String PREVIEW_SHOW    = "preview_show";
+                                               
+    public static final String SHOW_COMMENT    = "show_comment";
+    public static final String SHOW_INDEX      = "show_index";
+    public static final String SHOW_ITEM       = "show_item";
+                                               
+    public static final String TRACKS_INDEX    = "tracks_index";
+    public static final String TRACKS_ITEM     = "tracks_item";
+    public static final String TRACKS_MENU     = "tracks_menu";
+                                               
+    public static final String VENUE_INDEX     = "venue_index";
+    public static final String VENUE_ITEM      = "venue_item";
+    public static final String VENUE_RELATION  = "venue_relation";
+    public static final String VENUE_SHOW      = "venue_show";
+                                               
+    public static final String TABLE_HEADER    = "table_header";
+    public static final String TABLE_ROW       = "table_row";
+    public static final String TABLE_ROW_ALT   = "table_row_alt";
+    public static final String TABLE_FOOTER    = "table_footer";
         
     public static void main(String[] args) {
         if (args.length != 1) {
