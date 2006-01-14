122f7e78898b77287dc221b4344622480939a635
Greg Bolsinga <greg@bolsinga.com>
2006-01-14 20:58:27 +0000

    add itunes 'Protected' key word.
    git-svn-id: file:///svn/repository/web_generator/trunk@1602 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/itunes/converter/ITunes.java
+++ src/com/bolsinga/itunes/converter/ITunes.java
@@ -49,6 +49,7 @@ public class ITunes {
   private static final String TK_EPISODE_ORDER        = "Episode Order";
   private static final String TK_HAS_VIDEO            = "Has Video";
   private static final String TK_TV_SHOW              = "TV Show";
+  private static final String TK_PROTECTED            = "Protected";
     
   private static final String FORMAT_12_INCH_LP       = "12 Inch LP";
   private static final String FORMAT_12_INCH_EP       = "12 Inch EP";
@@ -148,6 +149,7 @@ public class ITunes {
     sITunesKeys.add(TK_EPISODE_ORDER);
     sITunesKeys.add(TK_HAS_VIDEO);
     sITunesKeys.add(TK_TV_SHOW);
+    sITunesKeys.add(TK_PROTECTED);
   }
         
   public static void addMusic(ObjectFactory objFactory, com.bolsinga.music.data.Music music, String itunesFile) throws JAXBException {
