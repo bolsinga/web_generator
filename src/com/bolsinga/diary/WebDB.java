930711534bc2aced3191d50ade11df55c0437399
Greg Bolsinga <greg@bolsinga.com>
2006-02-03 22:48:38 +0000

    Add calls that will write out diary and music XML files from the data read from the db. This will validate the objects created from the DB.
    git-svn-id: file:///svn/repository/web_generator/trunk@1613 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/diary/WebDB.java
+++ src/com/bolsinga/diary/WebDB.java
@@ -1,9 +1,16 @@
 package com.bolsinga.diary;
 
+import java.io.*;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
 import com.bolsinga.diary.data.*;
 import com.bolsinga.music.data.*;
 
 public class WebDB {
+  private static final boolean GENERATE_XML = false;
 
   public static void main(String[] args) {
     if (args.length != 4) {
@@ -19,11 +26,39 @@ public class WebDB {
   public static void generate(String user, String password, String outputDir) {
     Diary diary = Util.createDiary(user, password);
     WebDB.generate(diary, user, password, outputDir);
+
+    if (WebDB.GENERATE_XML) {
+      WebDB.export(diary);
+    }
   }
 
   public static void generate(Diary diary, String user, String password, String outputDir) {
     Music music = com.bolsinga.music.Util.createMusic(user, password);
     com.bolsinga.web.Encode encoder = com.bolsinga.web.Encode.getEncode(music, diary);                
     Web.generate(diary, music, encoder, outputDir);
+  }
+
+  private static void export(Diary diary) {
+    try {
+      File outputFile = new File("/tmp", "diary_db.xml");
+
+      JAXBContext jc = JAXBContext.newInstance("com.bolsinga.diary.data");
+      Marshaller m = jc.createMarshaller();
+      m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                        
+      OutputStream os = null;
+      try {
+        os = new FileOutputStream(outputFile);
+      } catch (IOException ioe) {
+        System.err.println(ioe);
+        ioe.printStackTrace();
+        System.exit(1);
+      }
+      m.marshal(diary, os);
+    } catch (Exception e) {
+      System.err.println(e);
+      e.printStackTrace();
+      System.exit(1);
+    }
   }
 }
--- src/com/bolsinga/music/WebDB.java
+++ src/com/bolsinga/music/WebDB.java
@@ -1,8 +1,15 @@
 package com.bolsinga.music;
 
+import java.io.*;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
 import com.bolsinga.music.data.*;
 
 public class WebDB {
+  private static final boolean GENERATE_XML = false;
 
   public static void main(String[] args) {
     if (args.length != 4) {
@@ -20,5 +27,33 @@ public class WebDB {
     com.bolsinga.web.Encode encoder = com.bolsinga.web.Encode.getEncode(music, null);
 
     Web.generate(music, encoder, outputDir);
+
+    if (WebDB.GENERATE_XML) {
+      WebDB.export(music);
+    }
+  }
+
+  private static void export(Music music) {
+    try {
+      File outputFile = new File("/tmp", "music_db.xml");
+
+      JAXBContext jc = JAXBContext.newInstance("com.bolsinga.music.data");
+      Marshaller m = jc.createMarshaller();
+      m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                        
+      OutputStream os = null;
+      try {
+        os = new FileOutputStream(outputFile);
+      } catch (IOException ioe) {
+        System.err.println(ioe);
+        ioe.printStackTrace();
+        System.exit(1);
+      }
+      m.marshal(music, os);
+    } catch (Exception e) {
+      System.err.println(e);
+      e.printStackTrace();
+      System.exit(1);
+    }
   }
 }
