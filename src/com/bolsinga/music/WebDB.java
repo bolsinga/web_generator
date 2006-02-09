3c9a8f04f56927e06530e512c9b64736ac3a8fdb
Greg Bolsinga <greg@bolsinga.com>
2006-02-09 17:15:34 +0000

    when generating XML from the DB, don't bother creating the web pages as well.
    git-svn-id: file:///svn/repository/web_generator/trunk@1623 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/music/WebDB.java
+++ src/com/bolsinga/music/WebDB.java
@@ -24,13 +24,15 @@ public class WebDB {
 
   public static void generate(String user, String password, String outputDir) {
     Music music = com.bolsinga.music.Util.createMusic(user, password);
-    com.bolsinga.web.Encode encoder = com.bolsinga.web.Encode.getEncode(music, null);
-
-    Web.generate(music, encoder, outputDir);
 
     if (WebDB.GENERATE_XML) {
       WebDB.export(music);
+      System.exit(0);
     }
+
+    com.bolsinga.web.Encode encoder = com.bolsinga.web.Encode.getEncode(music, null);
+
+    Web.generate(music, encoder, outputDir);
   }
 
   private static void export(Music music) {
