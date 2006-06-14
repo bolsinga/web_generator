ebf72bbd54f698a103b09cf418e486f07ea38609
Greg Bolsinga <greg@bolsinga.com>
2006-06-14 01:39:59 +0000

    update svn_export to use data given to it instead of calculating it itself.
    git-svn-id: file:///svn/repository/build_tools/trunk@1745 a1e501d5-4615-0410-8dc9-a46690435e56

---
+++ build_tools/svn_export.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+usage()
+{
+  echo "svn_export.sh repository project_dir revision src_dir"
+  echo "Error: $1"
+  exit 1
+}
+
+if_failure()
+{
+  if [ "$?" -ne 0 ] ; then
+    echo "Error: $1"
+    exit 1
+  fi
+}
+
+REPOS=$1
+if [ -z "$REPOS" ] ; then
+    usage "No repository"
+fi
+
+PROJ_DIR=$2
+if [ -z "$PROJ_DIR" ] ; then
+    usage "No project_dir"
+fi
+
+REVIS=$3
+if [ -z "$REVIS" ] ; then
+    usage "No revision"
+fi
+
+SRC_DIR=$4
+if [ -z "$SRC_DIR" ] ; then
+    usage "No build_root_dir"
+fi
+NAMED_DIR=`basename $SRC_DIR`
+SRC_DIR=`dirname $SRC_DIR`
+if [ ! -d "$SRC_DIR" ] ; then
+  mkdir -p $SRC_DIR
+fi
+SRC_DIR=`cd "$SRC_DIR" && pwd`
+
+cd $SRC_DIR ; svn export -q -r $REVIS file://$REPOS/$PROJ_DIR $NAMED_DIR
+if_failure "Can't export $PROJ_DIR"
