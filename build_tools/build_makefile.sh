14f0c48b74f7d401b8fcf88e357d7271b8426d81
Greg Bolsinga <greg@bolsinga.com>
2006-06-20 16:48:20 +0000

    fix some typos.
    git-svn-id: file:///svn/repository/build_tools/trunk@1767 a1e501d5-4615-0410-8dc9-a46690435e56

--- build_tools/build_makefile.sh
+++ build_tools/build_makefile.sh
@@ -32,7 +32,7 @@ if [ -z "$DST_DIR" ] ; then
   usage "No destination directory."
 fi
 
-cd $SRC_DIR ; make SRC_DIR=$SRC_DIR OBJ_DIR==$OBJ_DIR DST_DIR=$DST_DIR 1>&2
+cd $SRC_DIR ; make SRC_DIR=$SRC_DIR OBJ_DIR=$OBJ_DIR DST_DIR=$DST_DIR 1>&2
 if_failure "Make Build Failure!"
 
 exit 0
