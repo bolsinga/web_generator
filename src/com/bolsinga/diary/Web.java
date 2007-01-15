0eb516a1c6dd34fcaa47af1866e647179b2c7502
Greg Bolsinga <greg@bolsinga.com>
2007-01-15 05:24:43 +0000

    Have alt page use RecordDocumentCreator.
    git-svn-id: file:///svn/repository/web_generator/trunk@2294 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/diary/Web.java
+++ src/com/bolsinga/diary/Web.java
@@ -15,6 +15,125 @@ import org.apache.ecs.filter.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 
+abstract class DiaryRecordDocumentCreator extends com.bolsinga.web.RecordDocumentCreator {
+
+  protected final Diary fDiary;
+  private final int fStartYear;
+  
+  public DiaryRecordDocumentCreator(final Diary diary, final String outputDir, final boolean upOneLevel) {
+    super(com.bolsinga.web.Links.getLinks(upOneLevel), outputDir);
+    fDiary = diary;
+    fStartYear = Util.getStartYear(fDiary);
+  }
+  
+  protected String getCopyright() {
+    return com.bolsinga.web.Util.getCopyright(fStartYear);
+  }
+  
+  protected String getMainDivClass() {
+    return com.bolsinga.web.CSS.DOC_2_COL_BODY;
+  }
+}
+
+class SingleRecordDocumentCreator extends DiaryRecordDocumentCreator {
+
+  public static void createDocuments(final com.bolsinga.web.Backgrounder backgrounder, final com.bolsinga.web.Backgroundable backgroundable, final Diary diary, final String outputDir) {
+    SingleRecordDocumentCreator creator = new SingleRecordDocumentCreator(diary, outputDir);
+    creator.createAlt(backgrounder, backgroundable);
+  }
+  
+  private SingleRecordDocumentCreator(final Diary diary, final String outputDir) {
+    super(diary, outputDir, true);
+  }
+
+  protected void createAlt(final com.bolsinga.web.Backgrounder backgrounder, final com.bolsinga.web.Backgroundable backgroundable) {
+    backgrounder.execute(backgroundable, new Runnable() {
+      public void run() {
+        create(new com.bolsinga.web.RecordFactory() {
+          public Vector<com.bolsinga.web.Record> getRecords() {
+            Vector<com.bolsinga.web.Record> items = new Vector<com.bolsinga.web.Record>(1);
+            items.add(com.bolsinga.web.Record.createRecordSimple(getAlt()));
+            return items;
+          }
+          
+          public String getTitle() {
+            return com.bolsinga.web.Util.getResourceString("alttitle");
+          }
+          
+          public String getFilePath() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(com.bolsinga.web.Links.ALT_DIR);
+            sb.append(File.separator);
+            sb.append("index");
+            sb.append(com.bolsinga.web.Links.HTML_EXT);
+            return sb.toString();
+          }
+          
+          public com.bolsinga.web.Navigator getNavigator() {
+            return new com.bolsinga.web.Navigator(fLinks) {
+              public Element getColophonNavigator() {
+                return getCurrentNavigator();
+              }
+              
+              public Element getCurrentNavigator() {
+                return new StringElement(com.bolsinga.web.Util.getResourceString("alttitle"));
+              }
+            };
+          }
+        });
+      }
+    });
+  }
+
+  private Element getAlt() {
+    // Add data from diary colophon
+    UL list = com.bolsinga.web.Util.convertToUnOrderedList(fDiary.getColophon());
+    
+    Vector<Element> e = new Vector<Element>();
+    
+    {
+    // Add email contact
+    Object[] args = { com.bolsinga.web.Util.getSettings().getContact(), com.bolsinga.web.Util.getResourceString("program") };
+    if (com.bolsinga.web.Util.getDebugOutput()) {
+      args[1] = null;
+    }
+    e.addElement(new A( MessageFormat.format(com.bolsinga.web.Util.getResourceString("mailto"), args),
+                        com.bolsinga.web.Util.getResourceString("contact"))); // mailto: URL
+    }
+    
+    {
+    // Add the name of the program
+    Object[] args = { new Code(com.bolsinga.web.Util.getGenerator()).toString() };
+    e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("generatedby"), args)));
+    }
+    
+    if (!com.bolsinga.web.Util.getDebugOutput()) {
+      // Add date generated
+      Object[] args = { new Code(com.bolsinga.web.Util.nowUTC().getTime().toString()).toString() };
+      e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("generatedon"), args)));
+    }
+    
+    // Add the copyright
+    e.addElement(new StringElement(getCopyright()));
+
+    {
+    // RSS
+    Object[] args = { fLinks.getRSSLink().toString(), fLinks.getRSSAlt() };
+    e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("singlespace"), args)));
+    }
+    
+    {
+    // iCal
+    Object[] args = { fLinks.getICalLink().toString(), fLinks.getICalAlt() };
+    e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("singlespace"), args)));
+    }
+
+    Div d = com.bolsinga.web.Util.createDiv(com.bolsinga.web.CSS.COLOPHON);
+    d.addElement(com.bolsinga.web.Util.appendToUnorderedList(list, e));
+    return d;
+  }
+}
+
 class DiaryDocumentCreator extends com.bolsinga.web.MultiDocumentCreator {
   private final com.bolsinga.web.Encode fEncoder;
   private final java.util.Map<String, com.bolsinga.web.IndexPair> fEntryIndex;
@@ -223,11 +342,7 @@ public class Web implements com.bolsinga.web.Backgroundable {
       }
     });
 
-    backgrounder.execute(backgroundable, new Runnable() {
-      public void run() {
-        Web.generateAltPage(diary, links, startYear, outputDir);
-      }
-    });
+    SingleRecordDocumentCreator.createDocuments(backgrounder, backgroundable, diary, outputDir);
   }
 
   private static Element generateMainContent(final Diary diary, final Music music, final com.bolsinga.web.Links links, final com.bolsinga.web.Encode encoder) {
@@ -465,68 +580,6 @@ public class Web implements com.bolsinga.web.Backgroundable {
     }, startYear);
     page.add(Web.createOverviewTable(entryGroups, entryIndex, startYear));
     page.complete();
-  }
-
-  private static Element generateAltContent(final Diary diary, final com.bolsinga.web.Links links, final String program, final int startYear) {
-    // Add data from diary colophon
-    UL list = com.bolsinga.web.Util.convertToUnOrderedList(diary.getColophon());
-    
-    Vector<Element> e = new Vector<Element>();
-    
-    {
-    // Add email contact
-    Object[] args = { com.bolsinga.web.Util.getSettings().getContact(), program };
-    if (com.bolsinga.web.Util.getDebugOutput()) {
-      args[1] = null;
-    }
-    e.addElement(new A( MessageFormat.format(com.bolsinga.web.Util.getResourceString("mailto"), args),
-                        com.bolsinga.web.Util.getResourceString("contact"))); // mailto: URL
-    }
-    
-    {
-    // Add the name of the program
-    Object[] args = { new Code(com.bolsinga.web.Util.getGenerator()).toString() };
-    e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("generatedby"), args)));
-    }
-    
-    if (!com.bolsinga.web.Util.getDebugOutput()) {
-      // Add date generated
-      Object[] args = { new Code(com.bolsinga.web.Util.nowUTC().getTime().toString()).toString() };
-      e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("generatedon"), args)));
-    }
-    
-    // Add the copyright
-    e.addElement(new StringElement(com.bolsinga.web.Util.getCopyright(startYear)));
-
-    {
-    // RSS
-    Object[] args = { links.getRSSLink().toString(), links.getRSSAlt() };
-    e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("singlespace"), args)));
-    }
-    
-    {
-    // iCal
-    Object[] args = { links.getICalLink().toString(), links.getICalAlt() };
-    e.addElement(new StringElement(MessageFormat.format(com.bolsinga.web.Util.getResourceString("singlespace"), args)));
-    }
-
-    Div d = com.bolsinga.web.Util.createDiv(com.bolsinga.web.CSS.COLOPHON);
-    d.addElement(com.bolsinga.web.Util.appendToUnorderedList(list, e));
-    return d;
-  }
-
-  public static void generateAltPage(final Diary diary, final com.bolsinga.web.Links links, final int startYear, final String outputDir) {
-    com.bolsinga.web.SingleElementDocumentCreator altPage = new DiarySingleDocumentCreator(links, outputDir, "index", com.bolsinga.web.Util.getResourceString("alttitle"), com.bolsinga.web.Links.ALT_DIR, new com.bolsinga.web.Navigator(links) {
-      public Element getColophonNavigator() {
-        return getCurrentNavigator();
-      }
-      
-      public Element getCurrentNavigator() {
-        return new StringElement(com.bolsinga.web.Util.getResourceString("alttitle"));
-      }
-    }, startYear);
-    altPage.add(Web.generateAltContent(diary, links, com.bolsinga.web.Util.getResourceString("program"), startYear));
-    altPage.complete();
   }
 
   public static Element addItem(final com.bolsinga.web.Encode encoder, final Entry entry, final com.bolsinga.web.Links links, final boolean upOneLevel) {
