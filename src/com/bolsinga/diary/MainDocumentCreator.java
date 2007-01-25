cd90f94cf056ad570fe4c0637dbf1d9ba229c7ad
Greg Bolsinga <greg@bolsinga.com>
2007-01-25 23:19:23 +0000

    clean up imports
    git-svn-id: file:///svn/repository/web_generator/trunk@2334 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/diary/MainDocumentCreator.java
+++ src/com/bolsinga/diary/MainDocumentCreator.java
@@ -3,6 +3,8 @@ package com.bolsinga.diary;
 import com.bolsinga.diary.data.*;
 import com.bolsinga.music.data.*;
 
+import com.bolsinga.web.*;
+
 import java.util.*;
 
 import org.apache.ecs.*;
@@ -12,27 +14,27 @@ public class MainDocumentCreator extends DiaryEncoderRecordDocumentCreator {
 
   private final Music fMusic;
 
-  public static void createDocuments(final com.bolsinga.web.Backgrounder backgrounder, final com.bolsinga.web.Backgroundable backgroundable, final Diary diary, final String outputDir, final com.bolsinga.web.Encode encoder, final Music music) {
+  public static void createDocuments(final Backgrounder backgrounder, final Backgroundable backgroundable, final Diary diary, final String outputDir, final Encode encoder, final Music music) {
     MainDocumentCreator creator = new MainDocumentCreator(diary, outputDir, encoder, music);
     creator.create(backgrounder, backgroundable);
   }
   
-  private MainDocumentCreator(final Diary diary, final String outputDir, final com.bolsinga.web.Encode encoder, final Music music) {
+  private MainDocumentCreator(final Diary diary, final String outputDir, final Encode encoder, final Music music) {
     super(diary, outputDir, false, encoder);
     fMusic = music;
   }
   
   protected String getMainDivClass() {
-    return com.bolsinga.web.CSS.DOC_3_COL_BODY;
+    return CSS.DOC_3_COL_BODY;
   }
   
-  protected void create(final com.bolsinga.web.Backgrounder backgrounder, final com.bolsinga.web.Backgroundable backgroundable) {
+  protected void create(final Backgrounder backgrounder, final Backgroundable backgroundable) {
     backgrounder.execute(backgroundable, new Runnable() {
       public void run() {
-        create(new com.bolsinga.web.RecordFactory() {
-          public Vector<com.bolsinga.web.Record> getRecords() {
-            Vector<com.bolsinga.web.Record> items = new Vector<com.bolsinga.web.Record>(1);
-            items.add(com.bolsinga.web.Record.createRecordSimple(getMain()));
+        create(new RecordFactory() {
+          public Vector<Record> getRecords() {
+            Vector<Record> items = new Vector<Record>(1);
+            items.add(Record.createRecordSimple(getMain()));
             return items;
           }
           
@@ -43,11 +45,11 @@ public class MainDocumentCreator extends DiaryEncoderRecordDocumentCreator {
           public String getFilePath() {
             StringBuilder sb = new StringBuilder();
             sb.append("index");
-            sb.append(com.bolsinga.web.Links.HTML_EXT);
+            sb.append(Links.HTML_EXT);
             return sb.toString();
           }
           
-          public com.bolsinga.web.Navigator getNavigator() {
+          public Navigator getNavigator() {
             return com.bolsinga.music.Web.getMainPagePreviewNavigator(fMusic, fLinks);
           }
         });
@@ -58,35 +60,35 @@ public class MainDocumentCreator extends DiaryEncoderRecordDocumentCreator {
   private Element getMain() {
     ElementContainer ec = new ElementContainer();
     ec.addElement(getStaticHeader());
-    ec.addElement(com.bolsinga.web.Util.convertToUnOrderedList(fDiary.getHeader()));
+    ec.addElement(Util.convertToUnOrderedList(fDiary.getHeader()));
     ec.addElement(getDiary());
     return ec;
   }
 
   private static Div createStaticsOffsite(final String title, final String data) {
-    Div d = com.bolsinga.web.Util.createDiv(com.bolsinga.web.CSS.STATICS_OFFSITE);
+    Div d = Util.createDiv(CSS.STATICS_OFFSITE);
     d.addElement(new H4(title));
     if (data != null) {
-      d.addElement(com.bolsinga.web.Util.convertToUnOrderedList(data));
+      d.addElement(Util.convertToUnOrderedList(data));
     }
     return d;
   }
   
   private Div getStaticHeader() {
-    Div d = com.bolsinga.web.Util.createDiv(com.bolsinga.web.CSS.STATICS_HEADER);
-    d.addElement(MainDocumentCreator.createStaticsOffsite(com.bolsinga.web.Util.getSettings().getLinksTitle(), fDiary.getStatic()));
-    d.addElement(MainDocumentCreator.createStaticsOffsite(com.bolsinga.web.Util.getSettings().getFriendsTitle(), fDiary.getFriends()));
+    Div d = Util.createDiv(CSS.STATICS_HEADER);
+    d.addElement(MainDocumentCreator.createStaticsOffsite(Util.getSettings().getLinksTitle(), fDiary.getStatic()));
+    d.addElement(MainDocumentCreator.createStaticsOffsite(Util.getSettings().getFriendsTitle(), fDiary.getFriends()));
     return d;
   }
 
   private Element getDiary() {
-    Div diaryDiv = com.bolsinga.web.Util.createDiv(com.bolsinga.web.CSS.DOC_SUB);
+    Div diaryDiv = Util.createDiv(CSS.DOC_SUB);
 
-    int mainPageEntryCount = com.bolsinga.web.Util.getSettings().getDiaryCount().intValue();
+    int mainPageEntryCount = Util.getSettings().getDiaryCount().intValue();
     
     com.bolsinga.music.Lookup lookup = com.bolsinga.music.Lookup.getLookup(fMusic);
 
-    List<Object> items = com.bolsinga.web.Util.getRecentItems(mainPageEntryCount, fMusic, fDiary);
+    List<Object> items = Util.getRecentItems(mainPageEntryCount, fMusic, fDiary);
     for (Object o : items) {
       if (o instanceof com.bolsinga.diary.data.Entry) {
         diaryDiv.addElement(Web.addItem(fEncoder, (com.bolsinga.diary.data.Entry)o, fLinks, false));
