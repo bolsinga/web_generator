b253853b67fff18cb194fa2b48175355d4304d32
Greg Bolsinga <greg@bolsinga.com>
2007-01-28 19:52:07 +0000

    Create ITunesTest from ITunes.
    git-svn-id: file:///svn/repository/web_generator/trunk@2380 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/itunes/converter/ITunes.java
+++ src/com/bolsinga/itunes/converter/ITunes.java
@@ -77,62 +77,7 @@ public class ITunes {
   private static final String sLTReplacement = "&lt;";
   private static final Pattern sGTPattern = Pattern.compile(">");
   private static final String sGTReplacement = "&gt;";
-  
-  public static void main(String[] args) {
-    if (args.length != 2) {
-      System.out.println("Usage: ITunes [itunes] [output]");
-      System.exit(0);
-    }
-    
-    try {
-      ITunes.convert(args[0], args[1]);
-    } catch (ITunesException e) {
-      System.err.println(e);
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
-        
-  private static void convert(final String itunesFile, final String outputFile) throws ITunesException {          
-    Music music = ITunes.convert(itunesFile);
-
-    music.setTimestamp(com.bolsinga.web.Util.toXMLGregorianCalendar(com.bolsinga.web.Util.nowUTC()));
-
-    OutputStream os = null;
-    try {
-      os = new FileOutputStream(outputFile);
-    } catch (FileNotFoundException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Can't find file: ");
-      sb.append(outputFile);
-      throw new ITunesException(sb.toString(), e);
-    }
-    
-    try {
-      // Write out to the output file.
-      JAXBContext jc = JAXBContext.newInstance("com.bolsinga.music.data.xml");
-      Marshaller m = jc.createMarshaller();
-      m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-                        
-      m.marshal(music, os);
-    } catch (JAXBException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Can't marshall: ");
-      sb.append(os);
-      throw new ITunesException(sb.toString(), e);
-    }
-  }
-        
-  private static Music convert(final String itunesFile) throws ITunesException {
-    ObjectFactory objFactory = new ObjectFactory();
-                
-    Music music = objFactory.createMusic();
-
-    ITunes.addMusic(objFactory, music, itunesFile);
-                    
-    return music;
-  }
-        
+          
   private static void createKnownKeys() {
     sITunesKeys.add(TK_ALBUM);
     sITunesKeys.add(TK_ARTIST);
