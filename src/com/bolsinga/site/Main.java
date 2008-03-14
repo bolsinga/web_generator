b237db1fa9272ac770aa60a27e940a998e95f829
Greg Bolsinga <greg@bolsinga.com>
2008-03-14 03:58:47 +0000

    add option to output entries, venues, artists, labels, relations, songs, albums, shows indvidually as json files.
    git-svn-id: file:///svn/repository/web_generator/trunk@2744 a1e501d5-4615-0410-8dc9-a46690435e56

--- src/com/bolsinga/site/Main.java
+++ src/com/bolsinga/site/Main.java
@@ -4,6 +4,8 @@ import com.bolsinga.diary.*;
 import com.bolsinga.music.*;
 import com.bolsinga.web.*;
 
+import org.json.*;
+
 import java.util.*;
 
 public class Main implements Backgroundable {
@@ -51,6 +53,11 @@ public class Main implements Backgroundable {
         main.generateJSON(diaryFile, musicFile, itunes, shows, venue, sort, relations, comments, statics);
       } else if (command.equals("json-site")) {
         main.generateJSONSite(diaryFile, musicFile, output, cssFile, javaScriptFile);
+      } else if (command.startsWith("gen-json-")) {
+        String type = command.substring("gen-json-".length());
+        // In this case, diaryFile is the output directory.
+        String outputDir = diaryFile;
+        main.generateJSONFile(itunes, shows, venue, sort, relations, comments, statics, type, outputDir);
       } else {
         Main.usage(args, "Invalid action");
       }
@@ -110,6 +117,38 @@ public class Main implements Backgroundable {
     generateSite(music, diary, output, cssFile, javaScriptFile);
   }
 
+  private void generateJSONFile(final String itunes, final String shows, final String venue, final String sort, final String relations, final String comments, final String statics, final String type, final String outputDir) throws Exception {
+    final com.bolsinga.music.data.Music music = com.bolsinga.music.data.raw.Music.create(shows, venue, sort, relations, itunes);
+    final com.bolsinga.diary.data.Diary diary = com.bolsinga.diary.data.raw.Diary.create(comments, statics);
+
+    JSONObject json = new JSONObject();
+    try {
+      if (type.equals("entries")) {
+        json.put(type, com.bolsinga.diary.data.json.Diary.createEntriesJSON(diary.getEntries()));
+      } else if (type.equals("venues")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createVenuesJSON(music.getVenues()));
+      } else if (type.equals("artists")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createArtistsJSON(music.getArtists()));
+      } else if (type.equals("labels")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createLabelsJSON(music.getLabels()));
+      } else if (type.equals("relations")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createRelationsJSON(music.getRelations()));
+      } else if (type.equals("songs")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createSongsJSON(music.getSongs()));
+      } else if (type.equals("albums")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createAlbumsJSON(music.getAlbums()));
+      } else if (type.equals("shows")) {
+        json.put(type, com.bolsinga.music.data.json.Music.createShowsJSON(music.getShows()));
+      } else {
+        Main.usage(null, "Unknown JSON type: " + type);
+      }
+    } catch (JSONException e) {
+      throw new com.bolsinga.web.WebException("Can't export music to json", e);
+    }
+    java.io.File result = new java.io.File(outputDir, type + ".json");
+    com.bolsinga.web.Util.writeJSON(json, result.getPath());
+  }
+
   private void dumpSimilarArtists(final com.bolsinga.music.data.Music music) {
     String s;
     HashSet<String> bands = new HashSet<String>();
@@ -158,9 +197,11 @@ public class Main implements Backgroundable {
   private static void usage(final String[] badargs, final String reason) {
     System.out.println("Usage: Main [iTunes Music.xml] [shows.txt] [venuemap.txt] [bandsort.txt] [relations.txt] [comments.txt] [statics.txt] [settings.xml] [layout.css] [site.js] [diary input/output file] [music input/output file] [output.dir] <xml|xml-site|json|json-site|site>");
     System.out.println(reason);
-    System.out.println("Arguments:");
-    for (int i = 0; i < badargs.length; i++) {
-      System.out.print(badargs[i] + " ");
+    if (badargs != null) {
+      System.out.println("Arguments:");
+      for (int i = 0; i < badargs.length; i++) {
+        System.out.print(badargs[i] + " ");
+      }
     }
     System.out.println();
     System.exit(0);
