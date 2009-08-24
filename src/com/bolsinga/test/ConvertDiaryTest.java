b38241f72043d0edf63e9884767350f6137ad803
Greg Bolsinga <bolsinga@eno.local>
2009-08-23 21:30:29 -0700

    fix ConvertDiaryTest

--- src/com/bolsinga/test/ConvertDiaryTest.java
+++ src/com/bolsinga/test/ConvertDiaryTest.java
@@ -1,16 +1,20 @@
 package com.bolsinga.test;
 
+import com.bolsinga.web.*;
+
 public class ConvertDiaryTest {
 
   public static void main(String[] args) {
-    if (args.length != 3) {
-      System.out.println("Usage: ConvertDiaryTest [comments] [statics] [output]");
+    if (args.length != 4) {
+      System.out.println("Usage: ConvertDiaryTest [comments] [statics] [output] [settings.xml]");
       System.exit(0);
     }
     
     try {
-      com.bolsinga.diary.data.Diary diary = com.bolsinga.diary.data.raw.Diary.create(args[0], args[1]);
-      com.bolsinga.diary.data.xml.Diary.export(diary, args[2]);
+      Util.createSettings(args[0]);
+
+      com.bolsinga.diary.data.Diary diary = com.bolsinga.diary.data.raw.Diary.create(args[1], args[2]);
+      com.bolsinga.diary.data.xml.Diary.export(diary, args[3]);
     } catch (Exception e) {
       System.err.println(e);
       e.printStackTrace();
