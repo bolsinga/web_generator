a7792bf042638cead97608cf8abe5e3caa8b5943
Greg Bolsinga <bolsinga@eno.local>
2009-12-23 10:58:05 -0800

    Use new DynamicStatsRecordFactory class.

--- src/com/bolsinga/music/CityRecordDocumentCreator.java
+++ src/com/bolsinga/music/CityRecordDocumentCreator.java
@@ -12,7 +12,8 @@ import org.apache.ecs.html.*;
 public class CityRecordDocumentCreator extends MusicRecordDocumentCreator {
   private final String fTypeString = Util.getResourceString("city");
   private final Object fTypeArgs[] = { fTypeString };
-  
+  private int fTotal;
+    
   public static void createDocuments(final Backgrounder backgrounder, final Backgroundable backgroundable, final Music music, final String outputDir) {
     CityRecordDocumentCreator creator = new CityRecordDocumentCreator(music, outputDir);
     creator.createStats(backgrounder, backgroundable);
@@ -25,7 +26,8 @@ public class CityRecordDocumentCreator extends MusicRecordDocumentCreator {
   private void createStats(final Backgrounder backgrounder, final Backgroundable backgroundable) {
     backgrounder.execute(backgroundable, new Runnable() {
       public void run() {
-        create(new StatsRecordFactory() {
+        final Collection<String> items = fLookup.getCities();
+        create(new DynamicStatsRecordFactory() {
           public String getDirectory() {
             return Links.CITIES_DIR;
           }
@@ -46,118 +48,65 @@ public class CityRecordDocumentCreator extends MusicRecordDocumentCreator {
             };
           }
 
-          public Vector<Record> getRecords() throws com.bolsinga.web.WebException {
-            Vector<Record> records = super.getRecords();
-
-            Script script = new Script();
-            script.setType("text/javascript");
-            script.removeAttribute("language");
-
-            StringBuilder sb = new StringBuilder();
-            sb.append("window.addEventListener(\"load\",function(){");
-            sb.append("createStats(\"");
-            sb.append(CSS.TABLE_ROW_ALT);
-            sb.append("\",\"");
-            sb.append(CSS.TABLE_FOOTER);
-            sb.append("\",");
-            
-            final Collection<String> items = fLookup.getCities();
-            final ArrayList<org.json.JSONObject> values = new ArrayList<org.json.JSONObject>(items.size());
-
-            int total = trackStats(items, new StatsRecordFactory.StatsTracker() {
-                public void track(final String name, final String link, final int value) throws com.bolsinga.web.WebException {
-                    org.json.JSONObject json = new org.json.JSONObject();
-                    try {
-                        json.put("k", name);
-                        json.put("v", value);
-                    } catch (org.json.JSONException e) {
-                        throw new com.bolsinga.web.WebException("Can't create city stats json", e);
-                    }
-                    values.add(json);
-                }
-            });
-            org.json.JSONArray jarray = new org.json.JSONArray(values);
-            try {
-                if (com.bolsinga.web.Util.getPrettyOutput()) {
-                  sb.append(jarray.toString(2));
-                } else {
-                  sb.append(jarray.toString());
-                }
-            } catch (org.json.JSONException e) {
-                throw new com.bolsinga.web.WebException("Can't write city stats json array", e);
+            protected String getTableTitle() {
+                return MessageFormat.format(Util.getResourceString("showsby"), fTypeArgs);
             }
             
-            sb.append(",");
-            sb.append(total);
-            sb.append(");");
-
-            sb.append("},false);");
-            script.setTagText(sb.toString());
+            protected String getTableSummary() {
+                return Util.getResourceString("citystatsummary");
+            }
             
-            records.add(Record.createRecordSimple(script));
-
-            return records;
-          }
-
-          protected Table getTable() {
-            String tableTitle = MessageFormat.format(Util.getResourceString("showsby"), fTypeArgs);
-            Table table = Util.makeTable(tableTitle, Util.getResourceString("citystatsummary"), new TableHandler() {
-              public TR getHeaderRow() {
-                return new TR().addElement(new TH(fTypeString)).addElement(new TH("#")).addElement(new TH("%"));
-              }
-
-              public int getRowCount() {
-                return 0;
-              }
-              
-              public TR getRow(final int row) {
+            protected String getTableType() {
+                return fTypeString;
+            }
+            
+            protected int getStatsSize() {
+                return items.size();
+            }
+            
+            protected void generateStats(StatsRecordFactory.StatsTracker tracker) throws com.bolsinga.web.WebException {
+                HashMap<Integer, Collection<String>> cityCount = new HashMap<Integer, Collection<String>>();
+                String city = null;
+                int val;
+                Collection<String> stringCollection = null;
+                
+                for (String item : items) {
+                    val = fLookup.getShows(item).size();
+                    if (cityCount.containsKey(val)) {
+                        stringCollection = cityCount.get(val);
+                        stringCollection.add(item);
+                    } else {
+                        stringCollection = new HashSet<String>();
+                        stringCollection.add(item);
+                        cityCount.put(val, stringCollection);
+                    }
+                }
+                
+                List<Integer> keys = new Vector<Integer>(cityCount.keySet());
+                Collections.sort(keys);
+                Collections.reverse(keys);
+                
+                fTotal = 0;
+                for (int value : keys) {
+                    List<String> k = new Vector<String>(cityCount.get(value));
+                    Collections.sort(k);
+                    
+                    for (String j : k) {
+                        tracker.track(j, null, value);
+                        fTotal += value;
+                    }
+                }
+            }
+            
+            protected int getStatsTotal() {
+                return fTotal;
+            }
+            
+            protected String getStatsLinkPrefix() {
                 return null;
-              }
-              
-              public TR getFooterRow() {
-                return null;
-              }
-            });
-            table.setID("stats");
-            return table;
-          }
-      });
+            }
+        });
       }
     });
-  }
-  
-  private int trackStats(final Collection<String> items, final StatsRecordFactory.StatsTracker tracker) throws com.bolsinga.web.WebException {
-    HashMap<Integer, Collection<String>> cityCount = new HashMap<Integer, Collection<String>>();
-    String city = null;
-    int val;
-    Collection<String> stringCollection = null;
-
-    for (String item : items) {
-      val = fLookup.getShows(item).size();
-      if (cityCount.containsKey(val)) {
-        stringCollection = cityCount.get(val);
-        stringCollection.add(item);
-      } else {
-        stringCollection = new HashSet<String>();
-        stringCollection.add(item);
-        cityCount.put(val, stringCollection);
-      }
-    }
-                
-    List<Integer> keys = new Vector<Integer>(cityCount.keySet());
-    Collections.sort(keys);
-    Collections.reverse(keys);
-
-    int total = 0;
-    for (int value : keys) {
-      List<String> k = new Vector<String>(cityCount.get(value));
-      Collections.sort(k);
-
-      for (String j : k) {
-        tracker.track(j, null, value);
-        total += value;
-      }
-    }
-    return total;
   }
 }
