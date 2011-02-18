498706aa60cb412334e3be7c800da6b860df52b5
Greg Bolsinga <bolsinga@eno.local>
2011-02-17 22:32:14 -0800

    Revert "makeTable() is no longer used."
    This reverts commit 13500cca99ed448d90e3ea92019ca79e59f70930.

--- src/com/bolsinga/music/StatsRecordFactory.java
+++ src/com/bolsinga/music/StatsRecordFactory.java
@@ -29,4 +29,42 @@ public abstract class StatsRecordFactory implements RecordFactory {
   
   protected abstract String getDirectory();
   protected abstract Table getTable() throws com.bolsinga.web.WebException;
+
+  public static Table makeTable(final List<String> names, final List<Integer> values, final String caption, final String header, final String summary) {
+    int runningTotal = 0;
+    int i;
+    for (i = 0; i < values.size(); i++) {
+      runningTotal += values.get(i);
+    }
+    final int total = runningTotal;
+    
+    return Util.makeTable(caption, summary, new TableHandler() {
+      public TR getHeaderRow() {
+        return new TR().addElement(new TH(header)).addElement(new TH("#")).addElement(new TH("%"));
+      }
+
+      public int getRowCount() {
+        return values.size();
+      }
+      
+      public TR getRow(final int row) {
+        TR trow = new TR();
+        TH thh = new TH(names.get(row));
+        thh.setPrettyPrint(Util.getPrettyOutput());
+        trow.addElement(thh);
+        int value = values.get(row);
+        trow.addElement(new TD(Integer.toString(value)).setPrettyPrint(Util.getPrettyOutput()));
+        trow.addElement(new TD(Util.toString((double)value / total * 100.0)).setPrettyPrint(Util.getPrettyOutput()));
+        return trow;
+      }
+      
+      public TR getFooterRow() {
+        TR trow = new TR();
+        trow.addElement(new TH(Integer.toString(names.size())));
+        trow.addElement(new TH(Integer.toString(total)));
+        trow.addElement(new TH());
+        return trow;
+      }
+    });
+  }
 }
