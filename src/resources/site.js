b684ab144943e4f4d4cffc0833b4b6fe887aae9e
Greg Bolsinga <bolsinga@eno.local>
2011-02-17 22:32:22 -0800

    Revert "Handle links via 'l' property and optional title parameter for links."
    This reverts commit 427909b5bd03c5d09091dcd2fbbc8f8b150f71b6.

--- src/resources/site.js
+++ src/resources/site.js
@@ -5,7 +5,7 @@ function recordPopup(e) {
     location.href=value;
   }
 }
-function createStats(altClass, footerClass, vals, title) {
+function createStats(altClass, footerClass, vals) {
     var table = document.getElementById("stats");
     var total = 0;
     for (var v in vals) {
@@ -15,16 +15,7 @@ function createStats(altClass, footerClass, vals, title) {
         var tr = table.insertRow(table.rows.length);
         
         var th = document.createElement("th");
-        if (vals[v].l == null)
-            th.appendChild(document.createTextNode(vals[v].k));
-        else {
-            var a = document.createElement("a");
-            a.setAttribute("href", vals[v].l);
-            if (title != null)
-                a.setAttribute("title", title + vals[v].k);
-            a.appendChild(document.createTextNode(vals[v].k));
-            th.appendChild(a);
-        }
+        th.appendChild(document.createTextNode(vals[v].k));
         tr.appendChild(th);
         
         var td = document.createElement("td");
