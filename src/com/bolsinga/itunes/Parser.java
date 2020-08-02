43c05250671caf2b99123168fed1f30931c16fc7
Greg Bolsinga <bolsinga@mac.com>
2020-08-01 20:21:59 -0700

    refactor itunes xml parsing into separate file (#21)

--- src/com/bolsinga/itunes/Parser.java
+++ src/com/bolsinga/itunes/Parser.java
@@ -1,10 +1,7 @@
 package com.bolsinga.itunes;
 
-import java.io.*;
 import java.util.*;
 import java.util.regex.*;
-import javax.xml.parsers.*;
-import org.xml.sax.*;
 
 public class Parser {
   private static final String TK_GENRE_VOICE_MEMO = "Voice Memo";
@@ -26,7 +23,8 @@ public class Parser {
   private final HashSet<String> fArtistAlbumSet = new HashSet<String>();
 
   public List<Album> parse(final String itunesFile) throws ParserException {
-    List<Track> tracks = createTracks(itunesFile);
+    XMLParser xmlParser = new XMLParser();
+    List<Track> tracks = xmlParser.createTracks(itunesFile);
 
     addTracks(tracks);
 
@@ -201,84 +199,5 @@ public class Parser {
         a.setReleaseYear(albumYear);
       }
     }
-  }
-
-  private static Map<String, Object> createPlist(final String sourceFile) throws ParserException {
-    InputStream is = null;
-    try {
-      is = new FileInputStream(sourceFile);
-    } catch (FileNotFoundException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Can't find plist file: ");
-      sb.append(sourceFile);
-      throw new ParserException(sb.toString(), e);
-    }
-
-    SAXParserFactory parserFactory = SAXParserFactory.newInstance();
-    SAXParser parser = null;
-    try {
-      parser = parserFactory.newSAXParser();
-    } catch (ParserConfigurationException | SAXException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Can't create SAXParser");
-      throw new ParserException(sb.toString(), e);
-    }
-
-    XMLReader reader = null;
-    try {
-      reader = parser.getXMLReader();
-    } catch (SAXException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Can't create XMLReader");
-      throw new ParserException(sb.toString(), e);
-    }
-
-    ParserHandler handler = new ParserHandler();
-    reader.setContentHandler(handler);
-
-    InputSource source = new InputSource(is);
-    try {
-      reader.parse(source);
-    } catch (IOException | SAXException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Can't parse InputSource");
-      throw new ParserException(sb.toString(), e);
-    }
-
-    return handler.plist;
-  }
-
-  private static Map<String, Object> createTracksDict(final String sourceFile) throws ParserException {
-    Map<String, Object> plist = createPlist(sourceFile);
-
-    for (Map.Entry<String, Object> entry : plist.entrySet()) {
-      if (entry.getKey().equals("Tracks")) {
-        @SuppressWarnings("unchecked")
-        Map<String, Object> dict = (Map<String, Object>)entry.getValue();
-        return dict;
-      }
-    }
-    throw new ParserException("No Tracks key in plist: " + plist.toString());
-  }
-
-  private static List<Track> createTracks(final String sourceFile) throws ParserException {
-    Map<String, Object> tracksDict = createTracksDict(sourceFile);
-
-    ArrayList<Track> tracks = new ArrayList<Track>();
-
-    for (Object o : tracksDict.values()) {
-      @SuppressWarnings("unchecked")
-      Map<String, Object> trackDict = (Map<String, Object>)o;
-
-      Track track = new Track();
-
-      for (Map.Entry<String, Object> entry : trackDict.entrySet()) {
-        track.set(entry.getKey(), (String)entry.getValue());
-      }
-
-      tracks.add(track);
-    }
-
-    return tracks;
   }
 }
