9132dc64258a83790ac03f7411fb6e883cc1b598
Greg Bolsinga <bolsinga@mac.com>
2023-02-21 11:21:26 -0800

    Move output-dir to the second parameter (#122)

--- src/com/bolsinga/site/Main.java
+++ src/com/bolsinga/site/Main.java
@@ -18,6 +18,8 @@ public class Main implements Backgroundable {
     int i = 0;
     String command = args[i++];
 
+    String output = args[i++];
+
     String itunes = args[i++];
     String shows = args[i++];
     String venue = args[i++];
@@ -30,8 +32,6 @@ public class Main implements Backgroundable {
         
     String diaryFile = args[i++];
     String musicFile = args[i++];
-
-    String output = args[i++];
 
     Backgrounder backgrounder = Backgrounder.getBackgrounder();
     
